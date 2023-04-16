b5f8ccf995edb6fe224f9bf05e0daeaf3b5f2ec0
AOrps <aorbeandrews@gmail.com>
2023-04-16 11:12:51 -0400

    templated tools

--- website/website.go
+++ website/website.go
@@ -19,6 +19,17 @@ type Nav struct {
 	Name string
 }
 
+type Tool struct {
+	Title string
+	Cmd string
+	Desc string
+}
+
+type ToolPage struct {
+	Navs []Nav
+	Tools []Tool
+}
+
 // homepage: tools page of the website
 func homepage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseGlob("*.html"))
@@ -38,7 +49,24 @@ func tools(w http.ResponseWriter, r *http.Request) {
 		{Href: "/tools", Name: "Tools"},
 		{Href: "/why", Name: "Why"},
 	}
-	t.ExecuteTemplate(w, "tools", toolsNav)
+	tools := []Tool{
+		{Title: "Help", Cmd: "help", Desc: "This command shows a list of commands or help for one command"},
+		{Title: "Weather", Cmd: "wttr", Desc: "This command shows you the weather forecast for the next couple of days (with help of wttr.in)"},
+		{Title: "Dance", Cmd: "dance", Desc: "See Ice Spice's most famous video on the terminal with ascii art"},
+		{Title: "Logo", Cmd: "logo", Desc: "This command displays our logo from the homepage in ascii art"},
+		{Title: "Text", Cmd: "text", Desc: "This command sends a text message from a Miami number to person of your choice"},
+		{Title: "Name Servers", Cmd: "ns", Desc: "This command shows current name servers for url"},
+		{Title: "IP Address", Cmd: "ip", Desc: "This command shows both the IPv4 and IPv6 address of url"},
+		{Title: "Operating System", Cmd: "os", Desc: "This command checks the operating system & architecture of current device"},
+		//{Title: "", Cmd: "", Desc: ""},		
+	}
+
+	toolpage := ToolPage{
+		Navs: toolsNav,
+		Tools: tools,
+	}
+	
+	t.ExecuteTemplate(w, "tools", toolpage)
 }
 
 // why: why page of the website
