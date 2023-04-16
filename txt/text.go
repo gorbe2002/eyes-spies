adbed517c821f6d865be6d5e826f95daa9a78950
AOrps <aorbeandrews@gmail.com>
2023-04-16 07:21:47 -0400

    new error msg

--- txt/text.go
+++ txt/text.go
@@ -20,7 +20,8 @@ func SendBasicSms(msg, num string) {
 
 	resp, err := client.Api.CreateMessage(params)
 	if err != nil {
-		fmt.Println(err.Error())
+		// fmt.Println(err.Error())
+		fmt.Printf("Enter params for num & msg\nex:\n ./munch text --msg 'alexa show me this guy' --num '+1XXXYYYZZZ'\n\t for num put country code and 10 digits\n")
 	}
 	fmt.Println(resp)
 }
